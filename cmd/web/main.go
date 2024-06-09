2671067c6cea43bd89551352d6f91ec4d53f7aa7
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-06-09 16:05:44 +0300

    Set up DB, add draft Snippet model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,27 +1,42 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+
+	"snippetbox.xedelweiss.net/internal/models"
 )
 
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	addr := flag.String("addr", "127.0.0.1:4000", "HTTP network address")
+	dsn := flag.String("dsn", "username:userpass@/snippetbox?parseTime=true", "MySQL data source name")
 	flag.Parse()
 
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime|log.LUTC)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile|log.LUTC)
 
+	db, err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
+
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errorLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -31,6 +46,19 @@ func main() {
 	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	if err = db.Ping(); err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
 # Snippetbox
 
 Learning Golang with [Let's Go](https://lets-go.alexedwards.net/)
+
+## Docker
+
+Use `docker compose up --force-recreate --renew-anon-volumes` to recreate volume.
