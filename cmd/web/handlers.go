05c66aa80c702bcc36eb37313b91f6b0407052d1
Mykhailo Sverdlykivskyi <xedelweiss@gmail.com>
2024-06-15 18:17:47 +0300

    Use template data struct

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -68,7 +68,11 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 		app.serverError(w, err)
 	}
 
-	err = ts.ExecuteTemplate(w, "base", snippet)
+	data := &templateData{
+		Snippet: snippet,
+	}
+
+	err = ts.ExecuteTemplate(w, "base", data)
 	if err != nil {
 		app.serverError(w, err)
 	}
---
+++ cmd/web/templates.go
@@ -0,0 +1,7 @@
+package main
+
+import "snippetbox.xedelweiss.net/internal/models"
+
+type templateData struct {
+	Snippet *models.Snippet
+}
